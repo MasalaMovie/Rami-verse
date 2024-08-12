f3e89d4e8a1a834d8551340257f774d1592a2f82
MasalaMovie <168056962+MasalaMovie@users.noreply.github.com>
2024-08-12 14:34:38 +0545

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ if len(BOT_TOKEN) == 0:
 PORT = int(environ.get('PORT', '80'))
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/56381d1aa68e8670f34c6.jpg https://telegra.ph/file/6a3d8eab45a09104e8e2c.jpg https://telegra.ph/file/186d1a1f1562cfda57bce.jpg https://telegra.ph/file/fc1a420492d62978267b3.jpg https://telegra.ph/file/243c28093f885b47f8df2.jpg')).split()
 
 # Bot Admins
 ADMINS = environ.get('ADMINS', '')
